3d6b2aba3054005b3feb5ed6604629cfa48c41a9
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-02-16 14:12:21 -0600

    Add basic 12x12 map with blank tile asset and basic tooling script

---
+++ MassiveJungleDungeons/Assets/Scripts/EditorMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class EditorMenu
+{
+    [MenuItem("Tools/Assign Tile Material")]
+    public static void AssignTileMaterial()
+    {
+        var material = Resources.Load<Material>("Tile");
+
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach(GameObject t in tiles)
+            t.GetComponent<Renderer>().material = material;
+    }
+
+    [MenuItem("Tools/Assign Tile Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject t in tiles)
+            t.AddComponent<Tile>();
+    }
+}
