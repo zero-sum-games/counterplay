aa652df52117aeda80b79745edab7c87ce9aa345
Jeshua Johnson <jwj845@austin.eid.utexas.edu>
2020-05-03 15:07:51 -0500

    add UI menu system

---
+++ Counterplay/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    int currentScene;
+    public GameObject pauseObj;
+
+    GameObject[] destinations;
+    bool lerpFX;
+    
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        if (lerpFX)
+        {
+
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("CombatTesting");
+    }
+
+    public void ReturnToStart()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void CreditsScreen()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseObj.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseObj.SetActive(false);
+    }
+
+    public void RestartMatch()
+    {
+        SceneManager.LoadScene(currentScene);
+        
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+}
